317c0d9209a69611a1c346f92ae251d7f4a2fc14
gravlax7 <314761950+gravlax7@users.noreply.github.com>
2026-09-07 19:24:44 +0200

    Bundle fonts in the app

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,4 +1,6 @@
 import { render } from 'solid-js/web'
+import '@fontsource-variable/inter/wght.css'
+import '@fontsource-variable/jetbrains-mono/wght.css'
 import App from './App'
 import './theme/tokens.css'
 import './ui/ui.css'
